e556a18e7ca11cca00fccc5ce3fcbc50608e4f76
Анна <annaandreeva18@yandex.ru>
2023-07-15 22:37:47 +0400

    Ex16

--- lib/assertions.py
+++ lib/assertions.py
@@ -11,3 +11,22 @@ class Assertions:
 
         assert name in response_as_dict, f"Response doesn't have key '{name}'"
         assert response_as_dict[name] == expected_value, error_message
+
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name not in response_as_dict, f"Response shouldn't have key '{name}'. But it's present"
---
+++ tests/test_user_get.py
@@ -0,0 +1,24 @@
+import requests
+import pytest
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+class TestUserGet(BaseCase):
+    def test_get_another_user_details(self):
+        data = {
+            'password': '1234',
+            'email': 'vinkotov@example.com'
+        }
+
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        response2 = requests.get("https://playground.learnqa.ru/api/user/1", headers={"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
+
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")
